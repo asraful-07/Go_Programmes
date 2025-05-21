f770d35e25f12a7bdca7712174beb9e1ea735bab
asraful-07 <asraful0508@gmail.com>
2025-05-21 17:25:44 +0600

    somethig cheng it

--- main.go
+++ main.go
@@ -2,42 +2,27 @@ package main
 
 import "fmt"
 
-type Person struct {
-	name string
-	age int
-	hight int
-	class string
-}
+// type Person struct {
+// 	name string
+// 	age int
+// 	hight int
+// 	class string
+// }
 
-func (p Person) receiver() {
-  fmt.Println("Name :", p.name)
-  fmt.Println("Age :", p.age)
-  fmt.Println("Hight :", p.hight)
-  fmt.Println("Class :", p.class)
-}
+// func (p Person) receiver() {
+//   fmt.Println("Name :", p.name)
+//   fmt.Println("Age :", p.age)
+//   fmt.Println("Hight :", p.hight)
+//   fmt.Println("Class :", p.class)
+// }
+
 
 func main() {
-  Person1 := Person{
-	name : "Rahat",
-	age : 22,
-	hight : 167,
-	class : "CSE",
-  }
+ab := make([]int, 8)
 
-  fmt.Println("Name :", Person1.name)
-  fmt.Println("Age :", Person1.age)
-  fmt.Println("Hight :", Person1.hight)
-  fmt.Println("Class :", Person1.class)
-
-  var Person2 Person 
-
-  Person2 = Person{
-	name : "Sara",
-	age : 22,
-	hight : 160,
-	class : "CSE",
-  }
-Person2.receiver()
+fmt.Println(ab)
+fmt.Println(len(ab))
+fmt.Println(cap(ab))
 
 }
 
--- slice/make.go
+++ slice/make.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
  s := make([]int, 6, 7) // len: 6 || cap: 7
- s := make([]int, 3)
+ s := make([]int, 3)  // len: 3
 
  s[0] =3 
 
- fmt.Println(s)
+ fmt.Println(s)  // s = [3,0,0,0,0,0]
  fmt.Println(len(s))
  fmt.Println(cap(s))
 }
