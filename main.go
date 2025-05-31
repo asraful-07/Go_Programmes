26a3f2866e58a626cd089177181e0bf98374df24
asraful-07 <asraful0508@gmail.com>
2025-05-31 10:42:17 +0600

    today i am new working

--- main.go
+++ main.go
@@ -3,19 +3,7 @@ package main
 import "fmt"
 
 func add() (result int) {
-	fmt.Println("first", result)
 
- show := func() {
-		result = result + 10
-		fmt.Println("defer", result)
-	}
-
-	defer show()
-
-	result = 5 
-	fmt.Println("second", result)
-
-	return result
 }
 
 func main() {
