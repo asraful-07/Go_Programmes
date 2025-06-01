5375a26e83f4a3991aac89245f2446951973c0ba
asraful-07 <asraful0508@gmail.com>
2025-06-01 09:02:35 +0600

    mini project

--- main.go
+++ main.go
@@ -5,9 +5,37 @@ import "fmt"
 
 
 func main() {
-add(great)
+//  fmt.Println("Welcome to the quiz game")
+
+ var name string 
+ fmt.Printf("Enter Your Name: ")
+ fmt.Scan(&name)
+ fmt.Printf("Hello %v, Welcome to the game!\n", name)
+
+ var age uint 
+ fmt.Printf("Enter Your Age: ")
+ fmt.Scan(&age)
+
+ if age > 10 {
+   fmt.Println("Yay you can play game")
+ } else {
+   fmt.Println("You can't play game!")
+   return
+ }
+
+ fmt.Printf("What is better, RTX 3080 or RTX 3090? ")
+ var answer1 string
+ var answer2 string
+ fmt.Scan(&answer1, &answer2)
+ 
+ if answer1 + " " + answer2 == "RTX 3090" {
+   fmt.Println("correct")
+ } else {
+   fmt.Println("Incorrect!")
+ }
+
 }
 
 func init() {
-   fmt.Println("Hello I am first exc")
+   fmt.Println("Welcome to the quiz game")
 }
