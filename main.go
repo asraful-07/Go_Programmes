262a96220925f86ac66a621b07419bc72675a55c
asraful-07 <asraful0508@gmail.com>
2025-06-14 19:37:53 +0600

    slice somthing cheng

--- main.go
+++ main.go
@@ -2,39 +2,48 @@ package main
 
 import "fmt"
 
-type Cars struct {
-	name string  
-	price float64
-	color string
-	body string
-	rul []int
-}
-
-func modifyObject(p *Cars) {
-  p.color = "red"
-}
-
-func modifyArray(a *[4]string) {
- (*a)[2] = "H"
-}
-
 func main() {
-person := Cars{
-	name: "tesla",
-	price: 46.276,
-	color: "blue",
-	body: "tata",
-	rul: []int{1,2,3,4,5,6},
+	var s []int         // এখানে s হলো একটা খালি slice, মানে এর len = 0, cap = 0
+	s = append(s, 9)    // len = 1, cap = 1
+	s = append(s, 7)    // len = 2, cap = 2
+	s = append(s, 3)    // len = 3, cap = 4  <-- এখানে Go নিজে থেকে capacity বাড়িয়ে নেয়
+	
+	r := s              // এখন r এবং s একই backing array শেয়ার করে
+	s = append(s, 4)    // এখন s = [1 2 3 4], len = 4, cap = 4
+	s = append(s, 5)    // এখন capacity শেষ হয়ে গেছে (4 ছিল), তাই Go নতুন backing array বানায়
+	s = append(s, 5)   
+	
+	r = append(r, 4)    // r এখনো পুরাতন backing array ব্যবহার করছে, তাই ওখানে ৪ যোগ করে
+	s[0] = 10           // এটা নতুন backing array তে হচ্ছে, তাই r-এর উপর কোন প্রভাব নেই
+	
+
+
+    fmt.Println(s, len(s), cap(s))
+    fmt.Println(r, len(r), cap(r)) 
 }
 
-fmt.Println(person)
-modifyObject(&person)
-fmt.Println(person.rul[3])
 
-arr := [4]string{"A", "B", "C", "D"}
 
-fmt.Printf("string%s\n", arr)
-modifyArray(&arr)
-fmt.Println(arr)
 
-}
+
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// var arr []int
+
+// fmt.Println(arr)
+
+//  /*
+ 
+//  var s []int // []
+
+// s = append(s, 1, 2, 3) //[1], len: 3 , cap: 3
+
+// fmt.Println(s, len(s), cap(s)
+ 
+//  */
+
+// }
--- slice/empaty.go
+++ slice/empaty.go
@@ -11,14 +11,15 @@ func main() {
 	r := s              // এখন r এবং s একই backing array শেয়ার করে
 	s = append(s, 4)    // এখন s = [1 2 3 4], len = 4, cap = 4
 	s = append(s, 5)    // এখন capacity শেষ হয়ে গেছে (4 ছিল), তাই Go নতুন backing array বানায়
+	s = append(s, 6)   
 	
 	r = append(r, 4)    // r এখনো পুরাতন backing array ব্যবহার করছে, তাই ওখানে ৪ যোগ করে
 	s[0] = 10           // এটা নতুন backing array তে হচ্ছে, তাই r-এর উপর কোন প্রভাব নেই
 	
 
 
-    fmt.Println(s, len(s), cap(s))
-    fmt.Println(r, len(r), cap(r))
+    fmt.Println(s, len(s), cap(s))  //  [10 7 3 4 5 6] len(6)  cap(8)
+    fmt.Println(r, len(r), cap(r))  //  [9 7 3 4] len(4)  cap(4)
 }
 
 
