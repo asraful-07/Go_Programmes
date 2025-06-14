5afa324fcbfe0ed1cbb7d1923111a83d9a4316ca
asraful-07 <asraful0508@gmail.com>
2025-06-14 19:27:19 +0600

    today pointer something pratis

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ type Cars struct {
 	price float64
 	color string
 	body string
+	rul []int
 }
 
 func modifyObject(p *Cars) {
   p.color = "red"
+}
+
+func modifyArray(a *[4]string) {
+ (*a)[2] = "H"
 }
 
 func main() {
@@ -19,9 +24,17 @@ person := Cars{
 	price: 46.276,
 	color: "blue",
 	body: "tata",
+	rul: []int{1,2,3,4,5,6},
 }
+
 fmt.Println(person)
 modifyObject(&person)
-fmt.Println(person)
+fmt.Println(person.rul[3])
+
+arr := [4]string{"A", "B", "C", "D"}
+
+fmt.Printf("string%s\n", arr)
+modifyArray(&arr)
+fmt.Println(arr)
 
 }
