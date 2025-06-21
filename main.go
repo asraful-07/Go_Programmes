3f2e254e456e9e6616c1845fe12eea9f8a15b225
asraful-07 <asraful0508@gmail.com>
2025-06-21 10:59:17 +0600

    today i lear array

--- main.go
+++ main.go
@@ -1,26 +1,8 @@
 package main
 
-import (
-    "fmt"
-    "time"
-)
-
-var a = 10
-const b = 11
-
-func printHello(num int) {
-    fmt.Println("Hello Rahat", num)
-}
+import "fmt"
 
 func main() {
-    go printHello(1)
-    go printHello(2)
-    go printHello(3)
-    go printHello(4)
-    go printHello(5)
-    go printHello(6)
-
-    fmt.Println(a, "", b)
-
-    time.Sleep(1 * time.Second) 
+      var arr [8]int = [8]int{1, 3, 4, 5, 6, 7, 8, 9}
+    fmt.Println("arr", arr)
 }
