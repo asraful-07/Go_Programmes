2c8c0240a706a0fb18edfea7b006861240ee64ce
asraful-07 <asraful0508@gmail.com>
2025-06-29 10:45:34 +0600

    done

--- main.go
+++ main.go
@@ -5,13 +5,6 @@ import "fmt"
 
 
 func main() {
-x := 20
-p := &x // p ভেরিয়েবল, x এর এড্রেস রাখে
 
-*p = 30 // এড্রেস p তে থাকা মান (x) পরিবর্তন করে
-
-fmt.Println(x)  // 30
-fmt.Println(p)  // x এর এড্রেস
-fmt.Println(*p) // 30 (এড্রেসে থাকা মান)
 
 }
