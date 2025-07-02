7c7b6f17e7fc10bd8fd4ff1bcbac198c807d5975
asraful-07 <asraful0508@gmail.com>
2025-07-02 09:50:44 +0600

    something code

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import "fmt"
  
 func main() {
    x := 10
+   y := 12
 
    increment := func() int{
    x += 1
    return x
    }
 
+   decrement := func() int {
+      y -= 1
+      return y
+   }
+
    fmt.Println(increment())
    fmt.Println(increment())
+
+   fmt.Println(decrement())
+   fmt.Println(decrement())
 }
